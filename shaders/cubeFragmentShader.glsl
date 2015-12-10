f63623a97c3a15d1d90e53068eac475d283bea8d
Shchurov Alexey <alexiskhb@gmail.com>
2015-12-10 22:00:45 +1000

    optimized fragment shader

--- shaders/cubeFragmentShader.glsl
+++ shaders/cubeFragmentShader.glsl
@@ -6,7 +6,7 @@
 
 float ambientStrength = 0.5;
 float diffStrength = .8;
-float specStrength = .8;
+float specStrength = .4;
 
 uniform int lightamt;
 uniform vec3 eye;
@@ -35,7 +35,7 @@ struct Light {
     float spotAngle;
 };
 
-uniform Light lights[32];
+uniform Light lights[200];
 
 vec3 Diffuse(vec3 ldir, vec3 norm, vec3 color, float strength) {
 	float kdot = max(dot(ldir, norm), 0.0);
@@ -48,14 +48,17 @@ float Atten(vec3 lpos) {
 	return 1.0 / (A*D*D + B*D + C);
 }
 
-vec3 Specular(vec3 lpos, float lspec) {
-	vec3 ldir = normalize(lpos - fragPos);
-	vec3 eyee = normalize(eye - fragPos);
+vec3 ldir;
+vec3 eyee;
+vec3 refl;
+vec3 Specular(vec3 lpos) {
+	ldir = normalize(lpos - fragPos);
+	eyee = normalize(eye - fragPos);
 	float lambert = dot(ldir, normal);
 	if (lambert > 0.0) {
-		vec3 refl = reflect(-ldir, normal);
+		refl = reflect(-ldir, normal);
 		float specular = pow(max(dot(refl, eyee), 0.0), M.shine);
-		return lspec * M.specular * specular;
+		return M.specular * specular;
 	}
 	return vec3(0.0, 0.0, 0.0);
 }
@@ -66,35 +69,49 @@ vec3 diffuse = vec3(0., 0., 0.);
 vec3 specular = vec3(0., 0., 0.);
 vec3 result = vec3(0.0, 0.0, 0.0);
 
+int gtype;
+vec3 ltof;
+float atten;
+float D;
+float lambert;
+float spec;
 void main(void)
-{	
+{
 	M.diffuse = I * 0.4;
 	M.ambient = I * 0.4;
 	M.specular = I * 0.2;
-	M.shine = 8.0;
+	M.shine = 4.0;
 	
+	ambient = M.ambient * ambientStrength;
 	for(int i = 0; i < lightamt; i++) {
-		if (lights[i].disabled != 0)
-			continue;
-		if (lights[i].type == SPOT) {
-			vec3 ltof = normalize(fragPos - lights[i].pos);
-			float angle = dot(ltof, normalize(lights[i].dir));
-			ambient = M.ambient * ambientStrength; 
-			diffuse = Diffuse(normalize(lights[i].pos - fragPos), normal, lights[i].color, 1.);
-			specular = Specular(lights[i].pos, 1);
-			result += lights[i].intense*(ambient + diffuse + specular)*(angle > lights[i].spotAngle? 0.0 : Atten(lights[i].pos));
-			
-		} else if (lights[i].type == POINT) {
-			ambient = M.ambient * ambientStrength;
-			diffuse = Diffuse(normalize(lights[i].pos - fragPos), normal, lights[i].color, 1.);
-			specular = Specular(lights[i].pos, 1);
-			result += lights[i].intense*(ambient + diffuse + specular)*Atten(lights[i].pos);
-			
-		} else if (lights[i].type == DIR) {
-			ambient = M.ambient * ambientStrength;
-			diffuse = Diffuse(normalize(lights[i].dir), normal, lights[i].color, 1.);			
-			result += lights[i].intense*(ambient + diffuse);
+		gtype = lights[i].type;
+		D = length(lights[i].pos - fragPos);
+		if (gtype == SPOT) {
+			ltof = normalize(fragPos - lights[i].pos);
+			float angle = dot(ltof, normalize(lights[i].dir));			
+			atten = (angle > lights[i].spotAngle? 0.0 : (1.0 / (A*D*D + B*D + C)));
+			specular = I*0;
+			ldir = normalize(lights[i].pos - fragPos);
 		}
+		if (gtype == POINT) {
+			atten = 1.0 / (A*D*D + B*D + C);
+			ldir = normalize(lights[i].pos - fragPos);	
+			eyee = normalize(eye - fragPos);
+			lambert = dot(ldir, normal);
+			if (lambert > 0.0) {
+				refl = reflect(-ldir, normal);
+				spec = pow(max(dot(refl, eyee), 0.0), M.shine);
+			} else
+				spec = 0;
+			specular = M.specular * spec;
+		}
+		if (gtype == DIR) {
+			specular = I*0;			
+			ldir = normalize(lights[i].dir);
+			atten = 1;
+		}
+		diffuse = max(dot(ldir, normal), 0.0) * M.diffuse * diffStrength * lights[i].color;
+		result += lights[i].intense*(ambient + diffuse + specular)*atten;
 	}
 	color = vec4(result*cubeColor, 1.0);
 }
